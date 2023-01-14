bcfbeb51fe97b55e73085a1d22472051fa740fe5
Paul Joseph Beltran <paul-joseph.beltran.1@ens.etsmtl.ca>
2023-01-14 10:21:07 -0500

    add sort list team

--- competition/bot.py
+++ competition/bot.py
@@ -6,6 +6,16 @@ import strat_ennemies
 class Bot:
     def __init__(self):
         print("Initializing your super mega duper bot")
+    
+    def sortOtherTeams(self, gm: GameMessage):
+        teams = list()
+        teamId = gm.teamId
+        for otherTeamId in gm.teams:
+            if teamId != otherTeamId:
+                teams.append(gm.teamInfos.get(otherTeamId))
+        
+        teams.sort(key=lambda x : x.hp, reverse=True)
+        return teams
 
     def get_next_move(self, game_message: GameMessage):
         """
@@ -17,13 +27,13 @@ class Bot:
         
         heatmap = map_analyse.parcourir_chemins(game_message)
         position = map_analyse.get_meilleure_position(heatmap)
-        
+        otherTeams = self.sortOtherTeams(game_message)
 
         #actions.append(SellAction(Position(0, 0)))
         actions.append(BuildAction(TowerType.SPEAR_SHOOTER, position))
 
         if other_team_ids:
             ennemies_type = strat_ennemies.get_ennemies_type(game_message)
-            actions.append(SendReinforcementsAction(ennemies_type, other_team_ids[0]))
+            actions.append(SendReinforcementsAction(ennemies_type, otherTeams[0].id))
 
         return actions
